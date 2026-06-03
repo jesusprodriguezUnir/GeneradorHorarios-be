c41f6ce80f1fd6d2ecc86fc1f996c6619f5f9174
Jesús Pedro Rodriguez <jesuspedro.rodriguez@unir.net>
2026-06-03 22:47:07 +0200

    feat: actualizar versiones informativas en los archivos AssemblyInfo de pruebas de integración y unitarias

--- tests/api/Lectivo.UnitTests/obj/Debug/net8.0/Lectivo.UnitTests.AssemblyInfo.cs
+++ tests/api/Lectivo.UnitTests/obj/Debug/net8.0/Lectivo.UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lectivo.UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b51cdc287cf2737d2194e12f49942e45bd61e0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e081b662ae200d2230fc65021ff6fcbbd135601")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lectivo.UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lectivo.UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
